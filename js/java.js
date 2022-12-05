02391c70d4757e7f41696977cddd2989ea66cd07
psantacreu <pablosantacreu@yahoo.com.ar>
2022-12-05 01:36:46 -0300

    T10

--- js/java.js
+++ js/java.js
@@ -6,5 +6,50 @@ let saludo = "Les doy la bienvenida a ";
 let saludo1 = "y divertida.";
 let fraseCompleta = saludo + nombre + saludo1;
 
+alert(fraseCompleta);
+
 let frase = "JavaScript " + "es un lenguaje " + "esencial para crear " + "sitios web poderosos.";
+
+alert(frase);
+
+let numeroString1 = "10";
+let numeroString2 = "20";
+let numeroString3 = "30";
+let concatenaNumeros = numeroString1 +  " " + numeroString2 + " " + numeroString3;
+
+alert(concatenaNumeros);
+
+var age = prompt("Cual es tu edad");
+var name = prompt("cual es tu nombre");
+
+
+let edad;
+edad = prompt("Introduce tu edad");
+if (edad >= 18){
+alert("Puedes entrar, eres mayor de edad.");
+} 
+else {
+alert("No puedes entrar, eres menor de edad.");
+}
+
+
+sexo = prompt("introduce tu sexo");
+
+edad = prompt("Introduce tu edad");
+
+if (sexo = "femenino" && edad >= 20){
+    alert("Puedes entrar, eres mayor de edad.")
+}
+
+else if(sexo = "femenino" && edad < 20) {
+    alert("No puedes entrar, eres menor de edad.");
+    }
+
+if (sexo = "masculino" && edad >= 18){
+    alert("Puedes entrar, eres mayor de edad.")
+    }
+else if (sexo = "masculino" && edad > 18){
+    alert("No puedes entrar, eres menor de edad.");
+    }  
+
 
