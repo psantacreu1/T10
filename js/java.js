1ddfbe1f0803d4c912d957cb75af0ba8f51be76c
psantacreu <pablosantacreu@yahoo.com.ar>
2022-12-05 00:09:25 -0300

    T10

---
+++ js/java.js
@@ -0,0 +1,10 @@
+
+
+
+let nombre = "Programación Fácil ";
+let saludo = "Les doy la bienvenida a ";
+let saludo1 = "y divertida.";
+let fraseCompleta = saludo + nombre + saludo1;
+
+let frase = "JavaScript " + "es un lenguaje " + "esencial para crear " + "sitios web poderosos.";
+
