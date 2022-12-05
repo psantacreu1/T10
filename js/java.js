85acc503a6416b53470ae707e3eea50a13ea6761
psantacreu <pablosantacreu@yahoo.com.ar>
2022-12-05 01:51:25 -0300

    ....

--- js/java.js
+++ js/java.js
@@ -38,7 +38,7 @@ sexo = prompt("introduce tu sexo");
 edad = prompt("Introduce tu edad");
 
 if (sexo == "femenino" && edad >= 20){
-    alert("Puedes entrar, eres mayor de edad.")
+    alert("Puedes entrar, eres mayor de edad.");
 }
 
 else if(sexo == "femenino" && edad < 20) {
@@ -46,9 +46,9 @@ else if(sexo == "femenino" && edad < 20) {
     }
 
 if (sexo == "masculino" && edad >= 18){
-    alert("Puedes entrar, eres mayor de edad.")
+    alert("Puedes entrar, eres mayor de edad.");
     }
-else if (sexo == "masculino" && edad < 18){
+else if(sexo == "masculino" && edad < 18){
     alert("No puedes entrar, eres menor de edad.");
     }  
 
